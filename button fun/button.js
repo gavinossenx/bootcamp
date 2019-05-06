46fe67bda9188452c7247ddf32f21db3266b3c76
gavinossenx <gavin@ossenx.com>
2019-05-06 12:49:02 -0700

    images working

--- button fun/button.js
+++ button fun/button.js
@@ -1,8 +1,29 @@
+fetch('https://rickandmortyapi.com/api/character/?name=smith&status=alive')
+ .then(function(response) {
+    response.json()
+    .then((data) => {
+        let i;
+        for (i = 0; i < data.results.length; i++) { 
+         if (data.results[i].origin.name === "Earth (C-137)") {
+             familytree.push(data.results[i])
+         }
+        }
+        console.log(familytree)
+        for (i=0; i<familytree.length;i++){
+        document.getElementById("names").innerText +=" "+ familytree[i].name
+        document.getElementById("dimention").innerText += " " +familytree[i].origin.name
+        var node = document.createElement("img");
+        var image =document.getElementById("image")
+        node.setAttribute("src",familytree[i].image);
+        image.appendChild(node);
 
-var btn1 = document.getElementById("btn-1");
-console.log(btn1);
-var buttonclick = function(){
-    btn1.textContent = "yo";
-    console.log("hi");
-};
-console.log(this)
+       
+        
+        
+        
+        
+       
+  }
+    })
+  })
+  const familytree = [];
